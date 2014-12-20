87082af6fc6d6011b15856b647f6f7c4f870246f
Jayaganthan <jayaganthanjk@gmail.com>
2014-12-20 19:04:47 +0530

    added CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,21 +15,26 @@ class TasksController < ApplicationController
 
   # GET /tasks/new
   def new
+    @project = Project.find params[:project_id]
+    @story = Story.find params[:story_id]
     @task = Task.new
   end
 
   # GET /tasks/1/edit
   def edit
+    @project = Project.find params[:project_id]
+    @story = Story.find params[:story_id]
   end
 
   # POST /tasks
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-
+    @task.project_id = params[:project_id]
+    @task.story_id = params[:story_id]
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to project_story_tasks_path(project_id: @task.project_id, story_id: @task.story_id), notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -43,7 +48,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to project_story_tasks_path(project_id: @task.project_id, story_id: @task.story_id), notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
@@ -55,9 +60,11 @@ class TasksController < ApplicationController
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
+    story_id = @task.story_id
+    project_id = @task.project_id
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to project_story_tasks_path(project_id: :project_id, story_id: :story_id), notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -70,6 +77,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params[:task]
+      params.require(:task).permit(:name,:due_date,:estimated_time)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
 
   resources :projects do
-    resources :stories
+    resources :stories do
+      resources :tasks
+    end
   end
   devise_for :users
   get '/search', to: 'searches#index'
